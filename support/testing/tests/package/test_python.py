ba4eb6e079ca858f4a9bf86fa0bb9bdcc7bc3b34
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2016-10-14 15:28:00 +0200

    support/testing: add package tests
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ support/testing/tests/package/test_python.py
@@ -0,0 +1,23 @@
+import core.basetest
+import subprocess
+import os
+
+class TestPythonBase(core.basetest.BRTest):
+    config = core.basetest.basic_toolchain_config + """
+BR2_PACKAGE_PYTHON=y
+BR2_TARGET_ROOTFS_CPIO=y
+# BR2_TARGET_ROOTFS_TAR is not set
+"""
+
+    def test_run(self):
+        self.s.boot(arch="armv5", kernel="builtin",
+                    options=["-initrd", os.path.join(self.builddir, "images", "rootfs.cpio")])
+        self.s.login()
+        (r, s) = self.s.run("python --version 2>&1 | grep -q '^Python 2'")
+        self.assertEqual(s, 0)
+        (r, s) = self.s.run("python -c 'import math; math.floor(12.3)'")
+        self.assertEqual(s, 0)
+        (r, s) = self.s.run("python -c 'import ctypes; libc = ctypes.cdll.LoadLibrary(\"libc.so.1\"); print libc.time(None)'")
+        self.assertEqual(s, 0)
+        (r, s) = self.s.run("python -c 'import zlib'")
+        self.assertEqual(s, 1)
